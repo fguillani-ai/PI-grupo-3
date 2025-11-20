9469d3315d9ee0a453f04b4735e5d4b53edc8c04
Justo Fernandez <justofernandez@Justos-MBP.fibertel.com.ar>
2025-11-20 19:17:41 -0300

    Modifique un par mas de cosas del product

--- js/product.js
+++ js/product.js
@@ -5,6 +5,11 @@ let marca = document.querySelector('.marca');
 let precio = document.querySelector('.precio');
 let stock = document.querySelector('.stock');
 let id = new URLSearchParams(location.search).get('id');
+let cateLink = document.querySelector('.cate');
+let nombre = document.querySelector('.usarioR');
+let fecha = document.querySelector('.fechaR');
+let rating = document.querySelector('.estrellasR');
+let text = document.querySelector('.comentarioR');
 
 fetch(`https://dummyjson.com/products/${id}`)
     .then(function(respuesta) {
@@ -17,7 +22,16 @@ fetch(`https://dummyjson.com/products/${id}`)
         cate.innerText = data.category;
         marca.innerText = data.brand;
         precio.innerText = data.price;
-        stock.innerText = `Actualmente hay: ${data.stock} disponibles!`
+        stock.innerText = `Actualmente hay: ${data.stock} disponibles!` 
+        if(cateLink & data.category ){
+            cateLink.textContent = `categoria: ${data.category}`;
+            cateLink.href = `category.html?cate = ${data.category}`;
+        }
+        nombre.innerText = data.reviwerName;
+        fecha.innerText = data.date;
+        rating.innerText = data.rating;
+        text.innerText = data.comment;
+
     })
     .catch(function(error) {
         console.log('El error es: ' + error);
