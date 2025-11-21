86c1e63e8e5bcf1eeab63cd4638d4bb865e43bd4
Delfina Pigretti <delfinapigretti@Paulas-MacBook-Air-2.local>
2025-11-21 01:39:07 -0300

    arranque la validacion del register

--- js/register.js
+++ js/register.js
@@ -81,3 +81,28 @@ window.addEventListener('load', function() {
 });
 
 //terminar validacion
+window.addEventListener('load', function() {
+    let formulario = document.querySelector('.formularioR');
+    
+    let inputEmail = document.querySelector('.mail');
+    let inputPass = document.querySelector('.passw');
+    let contra2 = document.querySelector('contras');
+
+    formulario.addEventListener('submit', function(e) {
+        
+        let boton = e.submitter;
+        let accion = boton.getAttribute('formaction');
+
+        if (accion && accion.includes('login.html')) {
+            return; 
+        }
+
+        e.preventDefault();
+
+        let errores = false;
+        let valorEmail = inputEmail.value;
+        let valorContra = inputPass.value;
+        let contra2 = contra2.value;
+
+    });
+});
